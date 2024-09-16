eebd24195a5093e195ebcec34f746e4896afbe23
SulimanAlshain <98722966+SulimanAlshain@users.noreply.github.com>
2024-09-16 17:08:49 -0500

    Suliman Commit for Chapter_10 HandelError

---
+++ src/beginners_guide/chapter10/pages339_381/HandleError.java
@@ -0,0 +1,24 @@
+package beginners_guide.chapter10.pages339_381;
+
+
+import java.util.Random;
+
+class HandleError {
+    public static void main(String[] args) {
+        int a=0, b=0, c=0;
+        Random r = new Random();
+
+        for(int i=0; i<32000; i++) {
+            try {
+                b = r.nextInt();
+                c = r.nextInt();
+                a = 12345 / (b/c);
+            } catch (ArithmeticException e) {
+                System.out.println("Division by zero.");
+                a = 0; // set to zero and continue
+            }
+            System.out.println("a: " + a);
+        }
+    }
+}
+
